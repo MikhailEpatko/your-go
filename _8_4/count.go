37184ce1cccba62921edbc63ae09ec469eff1f09
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-02-15 09:51:44 +0300

    chapter 8.4

---
+++ _8_4/count.go
@@ -0,0 +1,33 @@
+package _8_4
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	STR_COUNT = 3
+)
+
+func CountVowelsInArray(strs [STR_COUNT]string) {
+	fmt.Println(countVowelsInArrayStr(strs))
+}
+
+func countVowelsInArrayStr(strs [STR_COUNT]string) string {
+	var counters [STR_COUNT]int
+	for i, s := range strs {
+		for _, c := range s {
+			switch c {
+			case 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
+				'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я':
+				counters[i]++
+			}
+		}
+	}
+	var stringCounters [STR_COUNT]string
+	for i, counter := range counters {
+		stringCounters[i] = strconv.Itoa(counter)
+	}
+	return strings.Join(stringCounters[:], " ")
+}
---
+++ _8_4/count_test.go
@@ -0,0 +1,14 @@
+package _8_4
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountVowelsInArrayStr(t *testing.T) {
+	a := assert.New(t)
+
+	want := "3 2 2"
+	got := countVowelsInArrayStr([STR_COUNT]string{"яблоко", "банан", "вишня"})
+	a.Equal(want, got)
+}
