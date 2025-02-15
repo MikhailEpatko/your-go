8a7cba622ebedf64609050919ef0d3450e0d5e6a
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-02-15 15:28:41 +0300

    chapter 9.2

---
+++ _9_4/magic.go
@@ -0,0 +1,45 @@
+package _9_4
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func printMagic(sl []int) {
+	fmt.Println(
+		magicString(
+			magicSlice(sl),
+		),
+	)
+}
+
+func magicString(numSlice []int) string {
+	strSlice := make([]string, len(numSlice))
+	for i, num := range numSlice {
+		strSlice[i] = strconv.Itoa(num)
+	}
+	return "[" + strings.Join(strSlice, ", ") + "]"
+}
+
+func magicSlice(sl []int) []int {
+	if len(sl) == 0 {
+		return sl
+	}
+	res := make([]int, len(sl))
+	for i := range sl {
+		res[i] = mult(sl, i)
+	}
+	return res
+}
+
+func mult(sl []int, excludeIndex int) int {
+	res := 1
+	for i, n := range sl {
+		if i == excludeIndex {
+			continue
+		}
+		res *= n
+	}
+	return res
+}
---
+++ _9_4/magic_test.go
@@ -0,0 +1,46 @@
+package _9_4
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMagicSlice(t *testing.T) {
+	a := assert.New(t)
+	assertSlices := func(t *testing.T, want, got []int) {
+		t.Helper()
+		a.Equal(want, got)
+	}
+
+	want := []int{24, 12, 8, 6}
+	got := magicSlice([]int{1, 2, 3, 4})
+	assertSlices(t, want, got)
+
+	want = []int{42, 35, 30, 210, 210}
+	got = magicSlice([]int{5, 6, 7, 1, 1})
+	assertSlices(t, want, got)
+
+	want = []int{0, -6, 0}
+	got = magicSlice([]int{2, 0, -3})
+	assertSlices(t, want, got)
+
+	want = []int{}
+	got = magicSlice([]int{})
+	assertSlices(t, want, got)
+}
+
+func TestMagicString(t *testing.T) {
+	a := assert.New(t)
+	assertStrings := func(t *testing.T, want, got string) {
+		t.Helper()
+		a.Equal(want, got)
+	}
+
+	want := "[1, 2, 3, 4]"
+	got := magicString([]int{1, 2, 3, 4})
+	assertStrings(t, want, got)
+
+	want = "[]"
+	got = magicString([]int{})
+	assertStrings(t, want, got)
+}
